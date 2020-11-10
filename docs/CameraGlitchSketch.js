74597efc0908f4fe7592c040b96950c1579c3667
cloudywolf16 <cabreros.joelmark@gmail.com>
2020-11-10 17:18:21 +0800

    .

--- docs/CameraGlitchSketch.js
+++ docs/CameraGlitchSketch.js
@@ -4,7 +4,7 @@
 
 var video;
 
-var vScale = 16;
+var vScale = 10;
 let font;
 
 let canvasDimension = { w: undefined, h: undefined };
@@ -72,22 +72,39 @@ function initInterval() {
 function draw() {
   background(bgColor);
   //stroke(strokeColor);
-  push();
   video.loadPixels();
   for (var y = 0; y < video.height; y++) {
     for (var x = 0; x < video.width; x++) {
-      var index = (video.width - x + (y * video.width)) * 4;
-      var r = video.pixels[index + 0];
-      var g = video.pixels[index + 1];
-      var b = video.pixels[index + 2];
-      var bright = (r + g + b) / 3;
-      var w = map(bright, 0, 255, 0, vScale);
-      //--RECTANGLE
+      let index = (video.width - x + (y * video.width)) * 4;
+      let r = video.pixels[index + 0];
+      let g = video.pixels[index + 1];
+      let b = video.pixels[index + 2];
+      let bright = (r + g + b) / 3;
+      let w = map(bright, 0, 255, 0, vScale);
+      let light = map(bright, 0, 255, 0.5, 1.5);
+
+      let angleNinety = 90;
+      //--RECTANGLE-1
       //noStroke();
       const rgb = [[255, 0, 0], [0, 255, 255], [255, 0, 255]];
       //rgb[floor(random(0,2))]
       //rectMode(CENTER);
       //rect(x * vScale, y * vScale, w);
+
+      //--RECTANGLE-2
+      // noStroke()
+      // colorMode(HSB,100);
+      // // if(w<=100){
+      // // fill(bright);}
+      // // else if(w<=150){
+      // // fill(bright); }
+      // // else if(w<150){
+      // //  }
+      // // else{
+      // //  }
+      // fill(100-light,100,100);
+      // rectMode(CENTER);
+      // rect(x * vScale, y * vScale+7, w);
 
       //--LINES
       //stroke(250);
@@ -114,16 +131,121 @@ function draw() {
 
 
       //--JAGGED-DIAGONAL-LINE
-      fill(r,g,b);
-      stroke(r,g,b);
-      strokeWeight(w * 0.25); 
-      quad(x * vScale - vScale, y * vScale - vScale, x * vScale - (vScale / 2), y * vScale - (vScale / 1.5), x * vScale + (vScale / 2), y * vScale + (vScale / 1.5), x * vScale + vScale, y * vScale + vScale);
+      //fill(r,g,b);
+      //stroke(r,g,b);
+      // fill(255);
+      // stroke(255);
+      // strokeWeight(w * 0.10); 
+      // quad(x * vScale - vScale, y * vScale - vScale, x * vScale - (vScale / 2) + (w*0.25), y * vScale - (vScale / 1.5), x * vScale + (vScale / 2), y * vScale + (vScale / 1.5), x * vScale + vScale, y * vScale + vScale);
 
+      //--ZIGZAG--x+y+w
+
+
+      //--ROTATING-SQUARES
+      // noStroke();
+      // push();
+      // fill(bright);
+      // translate(x * vScale - 8, y * vScale + 8);
+      // rotate(light);
+      // rectMode(CENTER);
+      // rect(0,0, vScale*0.75);
+      // pop();
+
+      //--RANDOM-LETTERS
+      // let chars = "ABCDEF00000000011111111";
+      // colorMode(HSB,358,100,100);
+      // fill(114,100,light);
+      // stroke(114,100,light);
+      // let letter = chars.charAt(Math.floor(Math.random() * chars.length));
+      // text(letter, x * vScale, y * vScale + 12);
+
+      //--TRIANGLE
+
+      // //colorMode(HSB,100);
+      // fill(100-light,100,100);
+      // stroke(100-light,100,100);
+      // fill(r,g,b);
+      // stroke(r,g,b);
+      // strokeWeight(light*0.065);
+      // push();
+      // rectMode(CENTER);
+      // triangle(x * vScale + (vScale*0.25), y * vScale + vScale, x * vScale + (vScale/2), y * vScale + (vScale*0.5), x * vScale + (vScale*0.75), y * vScale + vScale)
+      // pop();
+
+      fill(255);
+      stroke(255);
+      //fill(r,g,b);
+      //stroke(r,g,b);
+      //angleMode(DEGREES);
+      push();
+      rectMode(CENTER);
+      translate(x * vScale - 10, y * vScale + 8);
+      scale(light);
+
+      if (y % 2 != 0 && x % 2 != 0) {
+        let p1x = vScale * 0.35; let p1y = vScale * 0.25;
+        let p2x = vScale * 0.25; let p2y = vScale * 0.35;
+
+        let p3x = vScale * 0.65; let p3y = vScale * 0.75;
+        let p4x = vScale * 0.75; let p4y = vScale * 0.65;
+
+        bezier(vScale * 0.75, vScale * 0.25, p1x, p1y, p2x, p2y, vScale * 0.25, vScale * 0.75);
+        bezier(vScale * 0.75, vScale * 0.25, p3x, p3y, p4x, p4y, vScale * 0.25, vScale * 0.75);
+      }
+      else if (y % 2 != 0 && x % 2 == 0) {
+        let p1x = vScale * 0.65; let p1y = vScale * 0.25;
+        let p2x = vScale * 0.75; let p2y = vScale * 0.35;
+
+        let p3x = vScale * 0.25; let p3y = vScale * 0.65;
+        let p4x = vScale * 0.35; let p4y = vScale * 0.75;
+
+        bezier(vScale * 0.25, vScale * 0.25, p1x, p1y, p2x, p2y, vScale * 0.75, vScale * 0.75);
+        bezier(vScale * 0.25, vScale * 0.25, p3x, p3y, p4x, p4y, vScale * 0.75, vScale * 0.75);
+      }
+      else if (y % 2 == 0 && x % 2 != 0) {
+        let p1x = vScale * 0.65; let p1y = vScale * 0.25;
+        let p2x = vScale * 0.75; let p2y = vScale * 0.35;
+
+        let p3x = vScale * 0.25; let p3y = vScale * 0.65;
+        let p4x = vScale * 0.35; let p4y = vScale * 0.75;
+
+        bezier(vScale * 0.25, vScale * 0.25, p1x, p1y, p2x, p2y, vScale * 0.75, vScale * 0.75);
+        bezier(vScale * 0.25, vScale * 0.25, p3x, p3y, p4x, p4y, vScale * 0.75, vScale * 0.75);
+      }
+      else if (y % 2 == 0 && x % 2 == 0) {
+        let p1x = vScale * 0.35; let p1y = vScale * 0.25;
+        let p2x = vScale * 0.25; let p2y = vScale * 0.35;
+
+        let p3x = vScale * 0.65; let p3y = vScale * 0.75;
+        let p4x = vScale * 0.75; let p4y = vScale * 0.65;
+
+        bezier(vScale * 0.75, vScale * 0.25, p1x, p1y, p2x, p2y, vScale * 0.25, vScale * 0.75);
+        bezier(vScale * 0.75, vScale * 0.25, p3x, p3y, p4x, p4y, vScale * 0.25, vScale * 0.75);
+      }
+
+      pop();
 
       //noLoop();
     }
   }
-  pop();
+  //fill("lime");
+  //rect(0, 0, vScale);
+
+  //let p1 = vScale*0.35;
+  //let p2 = vScale*0.25;
+  //let p3 = vScale*0.25;
+  //let p4 = vScale*0.35;
+
+  // beginShape();
+  // rectMode(CENTER);
+  // fill(255);
+  // stroke("red")
+  // bezier(vScale*0.75, vScale*0.25, vScale*0.35, vScale*0.25, vScale*0.25, vScale*0.35, vScale*0.25, vScale*0.75);
+  // bezier(vScale*0.75, vScale*0.25, vScale*0.65, vScale*0.75, vScale*0.75, vScale*0.65, vScale*0.25, vScale*0.75);
+  // //fill("blue");
+  // //circle(p1, p2, 2);
+  // //circle(p3, p4, 2);
+  // endShape(CLOSE)
 }
 
 //--Select Pixel Color
